f2023de67c520c009751618a661ed6a2b3e527d5
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-18 13:03:46 +0200

    Summarize Spring Boot entrypoint

--- src/main/java/de/team7/swt/Application.java
+++ src/main/java/de/team7/swt/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Application class that contains the main-method and thus akts as the entrypoint into the application.
+ *
  * @author Vincent Nadoll
  */
 @SpringBootApplication
