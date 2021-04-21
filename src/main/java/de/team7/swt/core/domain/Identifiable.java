d990f49c4b8d09cbb32774fb5fbe9ddcbc71b923
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-21 09:54:05 +0200

    Introduce abstract identifier value object

---
+++ src/main/java/de/team7/swt/core/domain/Identifiable.java
@@ -0,0 +1,23 @@
+package de.team7.swt.core.domain;
+
+import org.springframework.data.domain.Persistable;
+
+import java.util.Objects;
+
+/**
+ * Convenience interface which assumes the implementing entity is new as long as {@link #getId()} returns
+ * {@literal null}.
+ *
+ * @param <ID> the type of the identifier
+ * @author Vincent Nadoll
+ */
+public interface Identifiable<ID> extends Persistable<ID> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    default boolean isNew() {
+        return Objects.isNull(getId());
+    }
+}
