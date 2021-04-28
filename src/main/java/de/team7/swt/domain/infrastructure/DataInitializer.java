ef28f924391616865f169bcaaec65722e81fef90
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-28 14:45:26 +0200

    Add callback interface for initializing data

---
+++ src/main/java/de/team7/swt/domain/infrastructure/DataInitializer.java
@@ -0,0 +1,28 @@
+package de.team7.swt.domain.infrastructure;
+
+import org.springframework.core.Ordered;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Callback interface to set up initial data on application startup.
+ *
+ * @author Vincent Nadoll
+ */
+@FunctionalInterface
+public interface DataInitializer extends Ordered {
+
+    int DEFAULT_ORDER = 0;
+
+    /**
+     * Called on application startup to trigger data initialization.
+     *
+     * @implNote Will run inside a transaction.
+     */
+    @Transactional
+    void initialize();
+
+    @Override
+    default int getOrder() {
+        return DEFAULT_ORDER;
+    }
+}
