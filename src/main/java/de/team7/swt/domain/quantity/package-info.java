c24e1486f2ab1f94743207a2344105b884defa27
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-04-29 23:48:54 +0200

    Mark quantity API as non-null

---
+++ src/main/java/de/team7/swt/domain/quantity/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Domain type for working with quantities consistently. This package largely contains classes altered classes from the
+ * salespoint framework.
+ *
+ * @author Vincent Nadoll
+ * @see <a href="https://github.com/st-tu-dresden/salespoint">Salespoint</a>
+ * @see de.team7.swt.domain.quantity.Quantity
+ */
+@org.springframework.lang.NonNullApi
+package de.team7.swt.domain.quantity;
