dd5a5cd2cef500d5ea1c03610610a92a84ba67d0
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-03 15:05:13 +0200

    Reformat code

--- src/main/java/de/team7/swt/domain/shared/Identifiable.java
+++ src/main/java/de/team7/swt/domain/shared/Identifiable.java
@@ -7,8 +7,8 @@ import java.util.Objects;
 import javax.persistence.Transient;
 
 /**
- * Simple extension of {@link Persistable} which assumes the implementing entity {@link #isNew()} in case
- * {@link #getId()} is not set (yet).
+ * Simple extension of {@link Persistable} which assumes the implementing entity {@link #isNew()} in case {@link
+ * #getId()} is not set (yet).
  *
  * @author Vincent Nadoll
  * @see org.springframework.data.jpa.repository.support.JpaPersistableEntityInformation
@@ -17,11 +17,11 @@ import javax.persistence.Transient;
 public interface Identifiable<ID extends Identifier> extends Persistable<ID> {
 
     /**
-     * {@inheritDoc}
-     * Must be {@link Transient} in order to ensure that no JPA provider complains because of a missing setter.
+     * {@inheritDoc} Must be {@link Transient} in order to ensure that no JPA provider complains because of a missing
+     * setter.
      *
      * @see <a href="https://jira.spring.io/browse/DATAJPA-622">Hibernate thinks AbstractPersistable.isNew() is a
-     * property</a>
+     *     property</a>
      */
     @Transient
     @JsonIgnore
