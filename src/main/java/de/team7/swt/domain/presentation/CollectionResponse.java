8abf094f64233f29c9fedf09a1c2817e24b3233b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-04 00:52:01 +0200

    Add generic JSON collection wrapper type

---
+++ src/main/java/de/team7/swt/domain/presentation/CollectionResponse.java
@@ -0,0 +1,20 @@
+package de.team7.swt.domain.presentation;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collection;
+
+/**
+ * Simple collection wrapper to ensue collection response bodies are always embedded in a JSON root object.
+ *
+ * @author Vincent Nadoll
+ */
+@RequiredArgsConstructor
+public class CollectionResponse {
+
+    @Getter(value = AccessLevel.PRIVATE, onMethod_ = @JsonGetter("_embedded"))
+    private final Collection<?> embedded;
+}
