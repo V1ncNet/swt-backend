54f05821c81d376a8654061709dd3d79e48bb0c2
PuffderZauberdrache <58983942+PuffderZauberdrache@users.noreply.github.com>
2021-05-04 13:47:40 +0200

    All classes extended with Product and MappedSuperclass

---
+++ src/main/java/de/team7/swt/model/Product.java
@@ -0,0 +1,36 @@
+package de.team7.swt.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.money.MonetaryAmount;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class Product {
+
+    @Id
+    private UUID productId;
+
+    @NonNull
+    private String name;
+
+    //@NonNull
+    //private MonetaryAmount price;
+
+    @NonNull
+    private String categories;
+
+    @NonNull
+    private String metric;
+}
+
+
