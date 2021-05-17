b60bdd24a49ae2026fabd414e12dd6456e364933
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-17 10:59:38 +0200

    Refactor shopping cart

---
+++ src/main/java/de/team7/swt/checkout/model/Cart.java
@@ -0,0 +1,54 @@
+package de.team7.swt.checkout.model;
+
+import de.team7.swt.domain.catalog.Product;
+import de.team7.swt.domain.quantity.Quantity;
+import org.javamoney.moneta.Money;
+import org.springframework.util.Assert;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import javax.money.MonetaryAmount;
+
+/**
+ * Abstraction of a shopping cart.
+ *
+ * @author Vincent Nadoll
+ */
+public class Cart {
+
+    private final Map<Product, CartItem> items = new LinkedHashMap<>();
+
+    /**
+     * Saves given {@link Product} and {@link Quantity} and creates or updates an existing {@link CartItem}.
+     *
+     * @param product  must not be {@literal null}
+     * @param quantity must not be {@literal null}
+     * @return saved {@link CartItem}
+     */
+    public CartItem save(Product product, Quantity quantity) {
+        Assert.notNull(product, "Product must not be null");
+        Assert.notNull(quantity, "Quantity must not be null");
+
+        return items.compute(product, saveWith(quantity));
+    }
+
+    private static BiFunction<Product, CartItem, CartItem> saveWith(Quantity quantity) {
+        return (product, cartItem) -> Objects.isNull(cartItem)
+            ? new CartItem(product, quantity)
+            : cartItem.add(quantity);
+    }
+
+    public void clear() {
+        items.clear();
+    }
+
+    public MonetaryAmount getTotal() {
+        return items.values()
+            .stream()
+            .map(CartItem::getPrice)
+            .reduce(MonetaryAmount::add)
+            .orElse(Money.of(0, "EUR"));
+    }
+}
---
+++ src/main/java/de/team7/swt/checkout/model/CartItem.java
@@ -0,0 +1,58 @@
+package de.team7.swt.checkout.model;
+
+import de.team7.swt.domain.catalog.Product;
+import de.team7.swt.domain.quantity.Quantity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import org.springframework.util.Assert;
+
+import java.util.UUID;
+import javax.money.MonetaryAmount;
+
+/**
+ * Immutable entity encapsulating a {@link Product} and its {@link Quantity}.
+ *
+ * @author Vincent Nadoll
+ */
+@Value
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CartItem {
+
+    UUID id;
+    Product product;
+    MonetaryAmount price;
+    Quantity quantity;
+
+    /**
+     * Creates a new cart items with given product and quantity.
+     *
+     * @param product  must not be {@literal null}
+     * @param quantity must not be {@literal null}
+     */
+    CartItem(Product product, Quantity quantity) {
+        this(UUID.randomUUID(), product, quantity);
+    }
+
+    private CartItem(UUID id, Product product, Quantity quantity) {
+        Assert.notNull(product, "Product must not be null");
+        Assert.notNull(quantity, "Quantity must not be null");
+        product.verify(quantity);
+
+        this.id = id;
+        this.product = product;
+        this.price = product.getPrice().multiply(quantity.getAmount());
+        this.quantity = quantity;
+    }
+
+    /**
+     * Creates a new cart item instance that has the given quantity added to this one.
+     *
+     * @param quantity must not be {@literal null}
+     * @return a new cart item instance
+     */
+    final CartItem add(Quantity quantity) {
+        Assert.notNull(quantity, "Quantity must not be null");
+        return new CartItem(this.id, this.product, this.quantity.add(quantity));
+    }
+}
