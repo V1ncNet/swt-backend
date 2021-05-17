22724eeb200c20469a586e5df78761f95b0299fd
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-17 13:12:57 +0200

    Make order persistable

--- src/main/java/de/team7/swt/checkout/model/Order.java
+++ src/main/java/de/team7/swt/checkout/model/Order.java
@@ -5,12 +5,24 @@ import de.team7.swt.domain.catalog.Product;
 import de.team7.swt.domain.quantity.Quantity;
 import de.team7.swt.domain.shared.AggregateRoot;
 import de.team7.swt.domain.shared.Identifier;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
  * An {@link AggregateRoot} representing an identifiable collection of items.
@@ -18,16 +30,19 @@ import java.util.UUID;
  * @author Vincent Nadoll
  */
 @Getter
+@Entity
+@Table(name = "_order") // 'order' is reserved keyword in SQL
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class Order extends AggregateRoot<Order.Id> implements Totalable<OrderItem> {
 
+    @EmbeddedId
+    @GeneratedValue(generator = "order-id")
+    @GenericGenerator(name = "order-id", strategy = "dyob-id")
     @JsonUnwrapped
     private final Order.Id id;
 
+    @OneToMany(cascade = CascadeType.ALL)
     private final List<OrderItem> orderLine = new ArrayList<>();
-
-    public Order() {
-        this.id = new Id();
-    }
 
     /**
      * Adds a new item from given {@link Product} and {@link Quantity} to this order.
@@ -47,14 +62,24 @@ public class Order extends AggregateRoot<Order.Id> implements Totalable<OrderIte
         return orderLine.iterator();
     }
 
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("id", id)
+            .append("orderLine", orderLine)
+            .toString();
+    }
+
     /**
      * Value object representing a order's primary identifier.
      *
      * @author Vincent Nadoll
      */
+    @Embeddable
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static final class Id extends Identifier {
-        public Id() {
-            super(UUID.randomUUID());
+        public Id(@NonNull UUID id) {
+            super(id);
         }
     }
 }
--- src/main/java/de/team7/swt/checkout/model/OrderItem.java
+++ src/main/java/de/team7/swt/checkout/model/OrderItem.java
@@ -9,11 +9,23 @@ import de.team7.swt.domain.shared.Identifier;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.util.Assert;
 
 import java.util.UUID;
 import javax.money.MonetaryAmount;
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 
 /**
  * An {@link de.team7.swt.domain.shared.Entity} representing {@link Product} and {@link Quantity} that is intended to be
@@ -21,19 +33,27 @@ import javax.money.MonetaryAmount;
  *
  * @author Vincent Nadoll
  */
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class OrderItem extends de.team7.swt.domain.shared.Entity<OrderItem.Id> implements Priced {
 
+    @EmbeddedId
+    @GeneratedValue(generator = "orderItem-id")
+    @GenericGenerator(name = "orderItem-id", strategy = "dyob-id")
     @JsonUnwrapped
     private final OrderItem.Id id;
 
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "product_id")) // Avoids ambiguous column id
     @JsonIgnore
     private Product.Id productId;
 
     private String productName;
 
     @NumberFormat(pattern = "0.00 ¤")
+    @Convert(disableConversion = true)
+    @Type(type = "de.team7.swt.domain.infrastructure.money.MonetaryAmountType")
     private MonetaryAmount price;
 
     private Quantity quantity;
@@ -45,18 +65,17 @@ public class OrderItem extends de.team7.swt.domain.shared.Entity<OrderItem.Id> i
      * @param quantity must not be {@literal null}
      */
     OrderItem(Product product, Quantity quantity) {
-        this(new Id(), product, quantity);
+        this(null, product, quantity);
     }
 
     /**
      * Creates a new order item with given ID, product and quantity.
      *
-     * @param id       must not be {@literal null}
+     * @param id       can be {@literal null}
      * @param product  must not be {@literal null}
      * @param quantity must not be {@literal null}
      */
     protected OrderItem(OrderItem.Id id, Product product, Quantity quantity) {
-        Assert.notNull(id, "Item ID must not be null");
         Assert.notNull(product, "Product must not be null");
         Assert.notNull(quantity, "Quantity must not be null");
         product.verify(quantity);
@@ -73,15 +92,27 @@ public class OrderItem extends de.team7.swt.domain.shared.Entity<OrderItem.Id> i
         return productId.toString();
     }
 
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("id", id)
+            .append("productId", productId)
+            .append("productName", productName)
+            .append("price", price)
+            .append("quantity", quantity)
+            .toString();
+    }
 
     /**
      * Value object representing an order item's primary identifier.
      *
      * @author Vincent Nadoll
      */
+    @Embeddable
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static final class Id extends Identifier {
-        public Id() {
-            super(UUID.randomUUID());
+        public Id(@NonNull UUID id) {
+            super(id);
         }
     }
 }
