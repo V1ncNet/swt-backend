b41ee12ccd4b928a09e4bcaab0f2135fe1bba11b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-20 14:31:31 +0200

    Update HTTP method

--- src/main/java/de/team7/swt/checkout/presentation/CartController.java
+++ src/main/java/de/team7/swt/checkout/presentation/CartController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -57,7 +58,7 @@ class CartController {
      * @param product must not be {@literal null}
      * @return 200 - updated cart
      */
-    @PostMapping
+    @PutMapping
     ResponseEntity<Cart> addItem(@RequestParam("product_id") Product product) {
         if (cartContainsCategoriesFrom(product)) {
             throw new ValidationException(String.format(
