654b3ca954b66a0b107b8f4be60afa6d39c83d76
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-05-28 12:20:56 +0200

    Validate negative quantity update requests

--- src/main/java/de/team7/swt/inventory/presentation/InventoryController.java
+++ src/main/java/de/team7/swt/inventory/presentation/InventoryController.java
@@ -13,8 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * Stateful MVC-Controller to manage the product stock.
@@ -34,18 +37,25 @@ class InventoryController {
      * @return 200
      */
     @GetMapping("/stock")
-    String stock(Model model) {
+    String stock(@RequestParam(value = "item_id", required = false) InventoryItem.Id itemId, Model model) {
         List<InventoryItem> stock = inventory.findAll();
         model.addAttribute("inventory", stock);
         model.addAttribute("formData", new ItemFormData());
+        Optional.ofNullable(itemId)
+            .flatMap(inventory::findById)
+            .ifPresent(addAttributeTo(model, "selected"));
         return "stock";
+    }
+
+    private static Consumer<Object> addAttributeTo(Model model, String attributeName) {
+        return attributeValue -> model.addAttribute(attributeName, attributeValue);
     }
 
     /**
      * Exposes an endpoint to update an item's quantity.
      *
      * @param formData submitted form data
-     * @return a redirection to {@link #stock(Model)}
+     * @return a redirection to {@link #stock(InventoryItem.Id, Model)}
      */
     @PostMapping("/stock")
     String editItem(@ModelAttribute ItemFormData formData) {
@@ -55,6 +65,9 @@ class InventoryController {
 
         Quantity quantity = product.from(formData.itemQuantityAmountOffset);
         item.increase(quantity);
+        if (item.getQuantity().isNegative()) {
+            return "redirect:/stock?insufficient_stock&item_id=" + formData.itemId;
+        }
         inventory.save(item);
 
         return "redirect:/stock#" + formData.itemId;
